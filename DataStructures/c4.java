3fbc57347ee3d167562ce4f59d8ce84fc7ab8d66
RohanJ <rohanjyoti@Rohans-MacBook-Pro.local>
2014-04-20 15:38:22 -0400

    Finished Question 2

--- DataStructures/c4.java
+++ DataStructures/c4.java
@@ -21,6 +21,7 @@ public class c4{
         SOP("");
         SOP("Possible functions:");
         SOP("q1\tQuestion 1");
+        SOP("q2\tQuestion 2");
         SOP("===============================");
         SOP("");
 
@@ -31,11 +32,12 @@ public class c4{
 
         String function = args[0];
         if(function.equals("q1")) q1(args);
+        else if(function.equals("q2")) q2(args);
         else SOP("ERROR: Unknown function");
     } 
 
     public static void q1(String[] args) throws Exception{
-        //Implement a function to check if a binary tree is balanced. For the purpose of
+        //Question 4.1: Implement a function to check if a binary tree is balanced. For the purpose of
         //this question, a balaced tree is defined to be a tree such that the heights of the
         //two subtrees of any node never differ by more than one
          SOP("Running q1");
@@ -56,7 +58,62 @@ public class c4{
 
         if(isBalanced(root3)) SOP("Tree 3 IS balanced");
         else SOP("Tree 3 is NOT balanced");
-    }   
+    } 
+
+    public static void q2(String[] args) throws Exception{
+        //Question 4.2: Given a directed graph, design an algorithm to find out whether there is a route
+        //between two nodes
+        SOP("Please look at src code for answer");
+
+        //This is a simple graph traversal/search problem.
+        //We can use DFS or BFS
+
+        //DFS vs BFS
+        //DFS explores iteratively the adjancies in order. This means it can get far from the original node.
+        //BFS explores using a queue and explores the nodes closest to the original node first
+        //Therefore, use BFS if you know the two nodes are in the same region
+
+        //DFS pseudocode
+        /*
+            void DFS(bNode root){
+                if(root == null) return;
+                visit(root);
+                root.visited = true;
+                for(bNode n : root.getAdjacent()){
+                    if(n.visited == false){
+                        DFS(root);
+                    }
+                }
+            }
+        */
+
+        //BFS pseudocode
+        /*
+            void BFS(bNode root){
+                if(root == null) return;
+                Queue mQ = new Queue();
+                visit(root);
+                root.visited = true;
+                mQ.enqueue(root);
+
+                while(! mQ.isEmpty() ){
+                    bNode next = mQ.dequeue();
+                    for(bNode n : next.getAjdacent()){
+                        if(n.visited == false){
+                            visit(n);
+                            n.visited = true;
+                            queue.enqueue(n);
+                        }
+                    }
+                }
+            }
+        */
+
+
+        //So to solve this problem, we will call one of the above graph traversal algorithms on the start node
+        //and our visit function will check if the node is the same as the node you are trying to get to.
+
+    }  
 
     public static boolean isBalanced(bNode root){
         if(getHeight(root) == -1) return false;
